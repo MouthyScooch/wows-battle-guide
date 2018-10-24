34dace1cbcbd3b53212a8893a57eeaa2c368c420
mouthyscooch <mouthyscooch@gmail.com>
2018-10-23 22:34:48 -0700

    removed /build from .gitignore for deployment test

---
+++ build/precache-manifest.62f982223c1f61e79cb510d05f878e8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "17aa87fdaa226d9c894d02d096cccd59",
+    "url": "/static/media/logo.17aa87fd.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0a2c81ea39eec7461fb3",
+    "url": "/static/js/main.0a2c81ea.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "0a2c81ea39eec7461fb3",
+    "url": "/static/css/main.a653d40c.chunk.css"
+  },
+  {
+    "revision": "924c7290c7cfaa17342eb6a4afa04e25",
+    "url": "/index.html"
+  }
+];
