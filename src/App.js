ed33d1569c8b7419963b76332caec02253b41055
mouthyscooch <mouthyscooch@gmail.com>
2018-10-24 22:31:49 -0700

    .

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
           <Title />
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            AB or BC?... How to know you're in low tier.
+            AB or BC?... How to know you're in low tier
           </p>
 
         </header>
