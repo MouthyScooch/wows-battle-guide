cc9aba468ded1047a8da677f4d8cb9bd2555e853
mouthyscooch <mouthyscooch@gmail.com>
2018-10-25 23:51:05 -0700

    add style library. bring in bootstrap reactstrap and a bootstrap theme to help expedite version 1's styles

--- src/components/title/title.js
+++ src/components/title/title.js
@@ -5,24 +5,51 @@ import ShipSearch from './ship-search.js';
 import ShipFilter from './ship-filter.js';
 import ShipList from './ship-list.js';
 
+import { Button, ButtonGroup, Container, Row, Col, Tooltip } from 'reactstrap';
+
 export default class Title extends React.Component {
   constructor (props) {
     super(props);
 
+    this.toggle = this.toggle.bind(this);
     this.state = {
       selectedShip: "",
       shipList: [],
       shipFilter: {},
+      tooltipOpen: false
     }
+  }
+
+  toggle() {
+    this.setState({
+      tooltipOpen: !this.state.tooltipOpen
+    });
   }
 
   render() {
     return (
       <div>
+      <Container>
+      <Row>
+      <Col>
+
         Title
+        <ButtonGroup>
+          <Button>26</Button>
+          <Button>26</Button>
+          <Button>26</Button>
+        </ButtonGroup>
+        <Button >Buttons</Button>
+        <p>Somewhere in here is a <span style={{textDecoration: "underline", color:"blue"}} href="#" id="TooltipExample">tooltip</span>.</p>
+        <Tooltip placement="right" isOpen={this.state.tooltipOpen} target="TooltipExample" toggle={this.toggle}>
+          Hello world!
+        </Tooltip>
         <ShipFilter />
         <ShipList />
         <ShipSearch />
+        </Col>
+        </Row>
+        </Container>
       </div>
     )
   }
