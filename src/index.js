4fcc991a48dcd5fa8c58c789c660f0adce8d358d
mouthyscooch <mouthyscooch@gmail.com>
2018-10-26 11:45:09 -0700

    .

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css'; // Bootstrap styles override theme
-import './index.css';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
